daea016a7eb819ce98abb44f715aa92dbbd1980f
David Santos <david.santos@byterev.com>
2021-10-29 18:13:38 +0100

    nodejs and express

---
+++ 2-express-tutorial/app.js
@@ -0,0 +1,61 @@
+const express = require('express')
+
+const app = express()
+
+const { products, people } = require('./data')
+
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+})
+
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product
+
+        return { id, name, image }
+    })
+    res.json(newProducts)
+})
+
+app.get('/api/products/:productId', (req, res) => {
+    const { productId } = req.params
+    const singleProduct = products.find((product) => product.id === Number(productId))
+
+    if (!singleProduct) {
+        return res.status(404).send('Product does not exist')
+    }
+    res.json(singleProduct)
+})
+
+app.get('/api/products/:productId/reviews/:review', (req, res) => {
+    console.log(req.params)
+    res.send('dummy')
+})
+
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query)
+    const { search, limit } = req.query
+
+    let sortedProducts = [...products]
+
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search)
+        })
+    }
+
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+
+    if (sortedProducts.length < 1) {
+        // res.status(200).send('no products matched your search')
+        return res.status(200).json({ success: true, data: [] })
+    }
+
+    res.status(200).json({ success: true, data: sortedProducts })
+})
+
+app.listen(5000, () => {
+    console.log('server is listening on port 5000...')
+})
